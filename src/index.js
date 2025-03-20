779a704cacc6edd44ca25ee057fa1aacb572046f
Cauteros974 <74265764+Cauteros974@users.noreply.github.com>
2025-03-20 14:49:07 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import GlobalStyle from "./GlobalStyle";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
